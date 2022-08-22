065c49d8d4666cd7d532f7da8e20286c5381c4fd
Naina Upadhyay <nanu02d8@gmail.com>
2022-08-22 23:03:02 +0530

    Task 2 day 14 completed

---
+++ DAY 14/TASK-2/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardsComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
