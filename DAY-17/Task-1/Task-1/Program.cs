fe588fef8aa1e6c0a02ac4faf993c2dc85a83821
Naina.Upadhyay <nanu02d8@gmail.com>
2022-08-25 13:06:39 +0530

    Task 1 day 17 completed

---
+++ DAY-17/Task-1/Task-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array methods
+            int[] arr = { 2, 45, 32, 10, 50 };
+            int a = 2, b = 4, d = 10;
+            double c = 5;
+
+            array arrobj = new array();
+
+            arrobj.printArray(arr);
+            Console.WriteLine();
+
+            //Array sort method
+            Array.Sort(arr);
+            arrobj.printArray(arr);
+            Console.WriteLine();
+
+            //Array reverse method
+            Array.Reverse(arr);
+            arrobj.printArray(arr);
+            Console.WriteLine();
+
+            //Method Overloading function calling
+            arrobj.add(a, b);
+            arrobj.add(a, b, d);
+            arrobj.add(d, c);
+        }
+    }
+}
---
+++ DAY-17/Task-1/Task-1/array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class array
+    {
+        //Constructor method
+        public array()
+        {
+            Console.WriteLine("This statement will be automatically called everytime when a class is instantiated!!");
+        }
+
+        public void printArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        //method overloading functions
+        public void add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        public void add(int x, double y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        public void add(int x, int y, int z)
+        {
+            Console.WriteLine(x + y + z);
+        }
+    }
+}
