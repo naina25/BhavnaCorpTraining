b52610ec4769f10fb73e01e3725eca5c00fb8949
Naina.Upadhyay <nanu02d8@gmail.com>
2022-10-07 10:57:00 +0530

    Assessment completed

--- Assesment/Assessment7/Assessment7/Controllers/Finance/FinanceController.cs
+++ Assesment/Assessment7/Assessment7/Controllers/Finance/FinanceController.cs
@@ -21,6 +21,39 @@ namespace Assessment7.Controllers.Finance
             List<Assessment7.Models.Finance> finance = _context.Finance.ToList();
             return View(finance);
         }
+        public IActionResult Details(int Id)
+        {
+            Assessment7.Models.Finance fin = _context.Finance.Where(p => p.TransactionId == Id).FirstOrDefault();
+            return View(fin);
+        }
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Assessment7.Models.Finance fin = _context.Finance.Where(p => p.TransactionId == Id).FirstOrDefault();
+            return View(fin);
+        }
+        [HttpPost]
+        public IActionResult Edit(Assessment7.Models.Finance fin)
+        {
+            _context.Attach(fin);
+            _context.Entry(fin).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("FinanceEntry");
+        }
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+             Assessment7.Models.Finance fin = _context.Finance.Where(p => p.TransactionId == Id).FirstOrDefault();
+            return View(fin);
+        }
+        [HttpPost]
+        public IActionResult Delete(Employees employee)
+        {
+            _context.Attach(employee);
+            _context.Entry(employee).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            _context.SaveChanges();
+            return RedirectToAction("EmpView");
+        }
         [HttpGet]
         public IActionResult Create()
         {
